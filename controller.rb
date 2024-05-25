55da3128a5ba757efae6bf37e4b5c3cb5a5d1ba0
Matheus <teteulopes2011@hotmail.com>
2024-05-25 01:11:28 -0300

    Adição update user model

--- controller.rb
+++ controller.rb
@@ -4,6 +4,7 @@ require_relative 'user'
 class MyController
   def self.index
     dados = User.where({login: '060599'})
+    User.update({login: '060599'}, {email: 'ovelhoeomar123@gmail.com'})
     return dados
   end
 end
--- user.rb
+++ user.rb
@@ -1,18 +1,16 @@
 class User
-    attr_reader :id, :login, :email  # Defina os atributos que deseja acessar
+    attr_reader :id, :login, :email  
   
     def initialize(attributes = {})
       @id = attributes['id']
       @login = attributes['login']
       @email = attributes['email']
-      # Adicione outros atributos conforme necessário
     end
   
     def self.where(conditions)
       begin
         client = Connection.connect
   
-        # Construir a consulta SQL com base nas condições fornecidas
         conditions_str = conditions.map { |key, value| "#{key} = '#{value}'" }.join(' AND ')
         sql = "SELECT * FROM users WHERE #{conditions_str}"
   
@@ -26,4 +24,21 @@ class User
         Connection.disconnect(client) if client
       end
     end
+
+    def self.update(conditions, set)
+        begin
+          cliente = Connection.connect
+          conditions_str = conditions.map { |key, value| "#{key} = '#{value}'"}.join(' AND ')
+          set_str = set.map { |key, value| "#{key} = '#{value}'"}.join(', ')
+          sql = "UPDATE users SET #{set_str} WHERE #{conditions_str}"
+          puts "SQL de atualização: #{sql}" # Adiciona este puts para debug
+          atualizacao = cliente.execute(sql)
+          atualizacao.do
+        rescue TinyTds::Error => e
+          puts "Erro ao atualizar o usuário: #{e.message}"
+        ensure
+          Connection.disconnect(cliente) if cliente
+        end
+      end
+
   end
